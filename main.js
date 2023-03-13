f47ee285e0808d6139881f6f40e53712712d6be8
NassimZoubeir <nassimz@outlook.fr>
2023-03-13 13:59:44 +0100

    ajout fichier texte

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
+const ts = 1;
+const publicKey = '1120682b8d282743b6e2d0b9b86dfa2b'
+const hash = "134de50e4c0b0e1ec4272725a8788ed6a79fcc18f1120682b8d282743b6e2d0b9b86dfa2b";
+  
 
-const url = 'https://gateway.marvel.com/v1/public/characters';
-const apiKey = '1120682b8d282743b6e2d0b9b86dfa2b';
-  
-  fetch(`${url}?apikey=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      // Faire quelque chose avec les données renvoyées par l'API
-      console.log(data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  
+fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
+.then((response) => response.json())
+.then((result) => console.log(result));
