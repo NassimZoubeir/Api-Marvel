30fccefef30553bbaf99031f7fcf9697d1570b64
NassimZoubeir <nassimz@outlook.fr>
2023-03-13 15:14:43 +0100

    appel api dans le select

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const ts = 1;
-const publicKey = '1120682b8d282743b6e2d0b9b86dfa2b'
-const hash = "134de50e4c0b0e1ec4272725a8788ed6a79fcc18f1120682b8d282743b6e2d0b9b86dfa2b";
-  
-
-fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
+fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
 .then((response) => response.json())
 .then((result) => console.log(result));
+
+
+const select = document.querySelector('#characters-select');
+const photoDiv = document.querySelector('#character-photo');
+
+fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
+.then((response) => response.json())
+.then((data) => {
+  const characters = data.data.results;
+  characters.forEach((character) => {
+    const option = document.createElement('option');
+    option.value = character.id;
+    option.text = character.name;
+
+    select.appendChild(option);
+  });
+
+  select.addEventListener('change', (event) => {
+    const selectedId = event.target.value;
+    const selectedCharacter = characters.find((character) => character.id === parseInt(selectedId));
+
+    const img = document.createElement('img');
+    img.src = `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`;
+
+    const nameHeading = document.querySelector('#character-name');
+    nameHeading.textContent = selectedCharacter.name;
+
+    const descriptionDiv = document.querySelector('#character-description');
+    descriptionDiv.textContent = selectedCharacter.description;
+
+    photoDiv.innerHTML = '';
+    photoDiv.appendChild(img);
+  });
+});
+
