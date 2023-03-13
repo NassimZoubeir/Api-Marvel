b7e90ff53dad1d533c8c87a1fb8bc284b9c09cde
NassimZoubeir <nassimz@outlook.fr>
2023-03-13 16:12:09 +0100

    ajout du css

--- main.js
+++ main.js
@@ -3,36 +3,58 @@ fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d2827
 .then((result) => console.log(result));
 
 
+// const select = document.querySelector('#characters-select');
+// const photoDiv = document.querySelector('#character-photo');
+
+// fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
+// .then((response) => response.json())
+// .then((data) => {
+//   const characters = data.data.results;
+//   characters.forEach((character) => {
+//     const option = document.createElement('option');
+//     option.value = character.id;
+//     option.text = character.name;
+
+//     select.appendChild(option);
+//   });
+
+//   select.addEventListener('change', (event) => {
+//     const selectedId = event.target.value;
+//     const selectedCharacter = characters.find((character) => character.id === parseInt(selectedId));
+
+//     const img = document.createElement('img');
+//     img.src = `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`;
+
+//     const nameHeading = document.querySelector('#character-name');
+//     nameHeading.textContent = selectedCharacter.name;
+
+//     const descriptionDiv = document.querySelector('#character-description');
+//     descriptionDiv.textContent = selectedCharacter.description;
+
+//     photoDiv.innerHTML = '';
+//     photoDiv.appendChild(img);
+//   });
+// });
+
 const select = document.querySelector('#characters-select');
 const photoDiv = document.querySelector('#character-photo');
 
 fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
-.then((response) => response.json())
-.then((data) => {
-  const characters = data.data.results;
-  characters.forEach((character) => {
-    const option = document.createElement('option');
-    option.value = character.id;
-    option.text = character.name;
+.then(response => response.json())
+.then(data => {
+const characters = data.data.results;
+for (const character of characters) {
+const option = new Option(character.name, character.id);
+select.add(option);
+}
+select.addEventListener('change', () => {
+  const selectedId = select.value;
+  const selectedCharacter = characters.find(character => character.id === parseInt(selectedId));
 
-    select.appendChild(option);
-  });
+  photoDiv.innerHTML = `<img src="${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}">`;
 
-  select.addEventListener('change', (event) => {
-    const selectedId = event.target.value;
-    const selectedCharacter = characters.find((character) => character.id === parseInt(selectedId));
-
-    const img = document.createElement('img');
-    img.src = `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`;
-
-    const nameHeading = document.querySelector('#character-name');
-    nameHeading.textContent = selectedCharacter.name;
-
-    const descriptionDiv = document.querySelector('#character-description');
-    descriptionDiv.textContent = selectedCharacter.description;
-
-    photoDiv.innerHTML = '';
-    photoDiv.appendChild(img);
-  });
+  document.querySelector('#character-name').textContent = selectedCharacter.name;
+  document.querySelector('#character-description').textContent = selectedCharacter.description;
 });
 
+})
