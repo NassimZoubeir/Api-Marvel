b992c59d91c8b758c0047f2025aa01bcd64e46da
NassimZoubeir <nassimz@outlook.fr>
2023-03-14 15:27:53 +0100

    ajout de la condition js

--- main.js
+++ main.js
@@ -4,55 +4,6 @@ fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d2827
 
 // ------------------------------------ PREMIERE METHODE --------------------------------------------------------------------------------------
 
-// // Sélectionne l'élément HTML avec l'ID "characters-select"
-// const select = document.querySelector('#characters-select');
-
-// // Sélectionne l'élément HTML avec l'ID "character-photo"
-// const photoDiv = document.querySelector('#character-photo');
-
-// // Effectue une requête GET vers l'API Marvel
-// fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
-//   // Traite la réponse de la requête en convertissant la réponse en JSON
-//   .then((response) => response.json())
-//   // Traite les données JSON retournées par l'API
-//   .then((data) => {
-//     // Extrayez les personnages de la réponse de l'API
-//     const characters = data.data.results;
-
-//     // Parcourt chaque personnage et crée une option dans le menu déroulant pour le sélectionner
-//     characters.forEach((character) => {
-//       const option = document.createElement('option');
-//       option.value = character.id;
-//       option.text = character.name;
-//       select.appendChild(option);
-//     });
-
-//     // Ajoute un écouteur d'événements pour le changement de sélection dans le menu déroulant
-//     select.addEventListener('change', (event) => {
-//       // Obtenez l'ID du personnage sélectionné
-//       const selectedId = event.target.value;
-//       // Trouvez le personnage correspondant à l'ID sélectionné
-//       const selectedCharacter = characters.find((character) => character.id === parseInt(selectedId));
-
-//       // Créez un élément img pour afficher l'image du personnage sélectionné
-//       const img = document.createElement('img');
-//       img.src = `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`;
-
-//       // Met à jour le nom et la description du personnage sélectionné
-//       const nameHeading = document.querySelector('#character-name');
-//       nameHeading.textContent = selectedCharacter.name;
-
-//       const descriptionDiv = document.querySelector('#character-description');
-//       descriptionDiv.textContent = selectedCharacter.description;
-
-//       // Efface l'élément enfant de l'élément photoDiv pour le remplacer par l'image sélectionnée
-//       photoDiv.innerHTML = '';
-//       photoDiv.appendChild(img);
-//     });
-//   });
-
-//--------------------------------- DEUXIEME METHODE -----------------------------------------------------------------------------------
-
 // Sélectionne l'élément HTML avec l'ID "characters-select"
 const select = document.querySelector('#characters-select');
 
@@ -62,31 +13,87 @@ const photoDiv = document.querySelector('#character-photo');
 // Effectue une requête GET vers l'API Marvel
 fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
   // Traite la réponse de la requête en convertissant la réponse en JSON
-  .then(response => response.json())
+  .then((response) => response.json())
   // Traite les données JSON retournées par l'API
-  .then(data => {
+  .then((data) => {
     // Extrayez les personnages de la réponse de l'API
     const characters = data.data.results;
 
-    // Pour chaque personnage, créez une option dans le menu déroulant pour le sélectionner
-    for (const character of characters) {
-      const option = new Option(character.name, character.id);
-      select.add(option);
-    }
+    // Parcourt chaque personnage et crée une option dans le menu déroulant pour le sélectionner
+    characters.forEach((character) => {
+      const option = document.createElement('option');
+      option.value = character.id;
+      option.text = character.name;
+      select.appendChild(option);
+
+      // Condition permettant d'afficher seulement les personnages ayant une description et une image
+      if (character.description && character.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") {
+                option.value = character.id;
+                option.text = character.name;
+                select.appendChild(option);
+            }
+    });
 
     // Ajoute un écouteur d'événements pour le changement de sélection dans le menu déroulant
-    select.addEventListener('change', () => {
+    select.addEventListener('change', (event) => {
       // Obtenez l'ID du personnage sélectionné
-      const selectedId = select.value;
+      const selectedId = event.target.value;
       // Trouvez le personnage correspondant à l'ID sélectionné
-      const selectedCharacter = characters.find(character => character.id === parseInt(selectedId));
+      const selectedCharacter = characters.find((character) => character.id === parseInt(selectedId));
 
-      // Affichez l'image du personnage sélectionné
-      photoDiv.innerHTML = `<img src="${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}">`;
+      // Créez un élément img pour afficher l'image du personnage sélectionné
+      const img = document.createElement('img');
+      img.src = `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`;
 
-      // Affichez le nom et la description du personnage sélectionné
-      document.querySelector('#character-name').textContent = selectedCharacter.name;
-      document.querySelector('#character-description').textContent = selectedCharacter.description;
+      // Met à jour le nom et la description du personnage sélectionné
+      const nameHeading = document.querySelector('#character-name');
+      nameHeading.textContent = selectedCharacter.name;
+
+      const descriptionDiv = document.querySelector('#character-description');
+      descriptionDiv.textContent = selectedCharacter.description;
+
+      // Efface l'élément enfant de l'élément photoDiv pour le remplacer par l'image sélectionnée
+      photoDiv.innerHTML = '';
+      photoDiv.appendChild(img);
     });
   });
+
+//--------------------------------- DEUXIEME METHODE -----------------------------------------------------------------------------------
+
+// // Sélectionne l'élément HTML avec l'ID "characters-select"
+// const select = document.querySelector('#characters-select');
+
+// // Sélectionne l'élément HTML avec l'ID "character-photo"
+// const photoDiv = document.querySelector('#character-photo');
+
+// // Effectue une requête GET vers l'API Marvel
+// fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1120682b8d282743b6e2d0b9b86dfa2b&hash=f35698b40ba34103b57af598a88487fc")
+//   // Traite la réponse de la requête en convertissant la réponse en JSON
+//   .then(response => response.json())
+//   // Traite les données JSON retournées par l'API
+//   .then(data => {
+//     // Extrayez les personnages de la réponse de l'API
+//     const characters = data.data.results;
+
+//     // Pour chaque personnage, créez une option dans le menu déroulant pour le sélectionner
+//     for (const character of characters) {
+//       const option = new Option(character.name, character.id);
+//       select.add(option);
+//     }
+
+//     // Ajoute un écouteur d'événements pour le changement de sélection dans le menu déroulant
+//     select.addEventListener('change', () => {
+//       // Obtenez l'ID du personnage sélectionné
+//       const selectedId = select.value;
+//       // Trouvez le personnage correspondant à l'ID sélectionné
+//       const selectedCharacter = characters.find(character => character.id === parseInt(selectedId));
+
+//       // Affichez l'image du personnage sélectionné
+//       photoDiv.innerHTML = `<img src="${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}">`;
+
+//       // Affichez le nom et la description du personnage sélectionné
+//       document.querySelector('#character-name').textContent = selectedCharacter.name;
+//       document.querySelector('#character-description').textContent = selectedCharacter.description;
+//     });
+//   });
 
